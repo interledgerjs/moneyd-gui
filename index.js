563640a3e637b473f689f32750d6e03ae6dccf57
Ben Sharafian <benms64@gmail.com>
2018-04-06 13:53:27 -0700

    fix: various

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const router = require('koa-router')()
+const parser = require('koa-bodyparser')()
 const app = new Koa()
 
 const fs = require('fs-extra')
@@ -10,20 +11,67 @@ const views = require('koa-views')(path.resolve(__dirname, 'views'), {
 
 const fetch = require('node-fetch')
 const makePlugin = require('ilp-plugin')
+const plugin = makePlugin()
+const ILDCP = require('ilp-protocol-ildcp')
+const SPSP = require('ilp-protocol-spsp')
 
 const ADMIN_API_PORT = process.env.ADMIN_API_PORT || 7769
+const ADMIN_COMMANDS = {
+  status: true,
+  routing: true,
+  accounts: true,
+  balance: true,
+  rates: true,
+  stats: true
+}
 
 router.get('/', async ctx => {
   await ctx.render('index')
 })
 
-router.get('/api/:command', async ctx => {
-  const res = await fetch(`http://localhost:${ADMIN_API_PORT}/` + ctx.params.command, {
-    method: 'GET',
-  })
+router.post('/actions/send/query', async ctx => {
+  const { receiver } = ctx.request.body
+  const query = await SPSP.query(receiver)
+  query.sharedSecret = query.sharedSecret.toString('base64')
+  ctx.body = query
+})
 
-  const locals = await res.json()
-  locals._root = Object.assign({}, locals) // required for rates
+router.post('/actions/send/currency', async ctx => {
+  await plugin.connect()
+  ctx.body = await ILDCP.fetch(plugin.sendData.bind(plugin))
+})
+
+router.post('/actions/send', async ctx => {
+  const { receiver, amount } = ctx.request.body
+
+  try {
+    await SPSP.pay(plugin, {
+      receiver,
+      sourceAmount: amount
+    })
+  } catch (e) {
+    ctx.body = {
+      success: false,
+      message: e.message,
+      stack: e.stack
+    }
+    return
+  }
+
+  ctx.body = { success: true }
+})
+
+router.get('/api/:command', async ctx => {
+  locals = {}
+
+  if (ctx.params.command in ADMIN_COMMANDS) {
+    const res = await fetch(`http://localhost:${ADMIN_API_PORT}/` + ctx.params.command, {
+      method: 'GET',
+    })
+
+    locals = await res.json()
+    locals._root = Object.assign({}, locals) // required for rates
+  }
 
   await ctx.render(ctx.params.command, locals)
 })
@@ -33,7 +81,13 @@ router.get('/index.js', async ctx => {
   ctx.body = await fs.readFile(path.resolve(__dirname, 'src', 'index.js'))
 })
 
+router.get('/send.js', async ctx => {
+  ctx.set('Content-Type', 'text/javascript')
+  ctx.body = await fs.readFile(path.resolve(__dirname, 'src', 'send.js'))
+})
+
 app
+  .use(parser)
   .use(views)
   .use(router.routes())
   .use(router.allowedMethods())
