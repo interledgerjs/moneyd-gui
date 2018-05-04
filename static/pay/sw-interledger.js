d5b26d9c62942ee4a3cfddb4ac592b6a59dc35c2
Andros Wong <androswong@awong-lm.local>
2018-05-03 17:25:55 -0700

    Successfully store to indexDB

--- static/pay/sw-interledger.js
+++ static/pay/sw-interledger.js
@@ -14,7 +14,8 @@ self.addEventListener('paymentrequest', function (e) {
   // we send the user through the interledger flow.
   let url = 'http://localhost:7770/pay/interledger.html'
   if (e.methodData[0].supportedMethods[0].indexOf('interledger') === -1) {
-    window.alert('Interledger not supported')
+    console.log('Interledger not supported')
+    return false
   }
   e.openWindow(url)
     .then(windowClient => {
@@ -47,8 +48,6 @@ function sendPaymentRequest () {
   }
 
   clients.matchAll(options).then(function (clientList) {
-    console.log("clientList", clientList)
-    console.log(payment_request_event)
     for (let i = 0; i < clientList.length; i++) {
       clientList[i].postMessage({
         paymentRequestId: payment_request_event.paymentRequestId,
@@ -56,7 +55,46 @@ function sendPaymentRequest () {
         instrumentKey: payment_request_event.instrumentKey,
         methodData: payment_request_event.methodData
       })
-      // white list paymentRequestOrigin to always if below a certain amount
+      // white list paymentRequestOrigin to always if below a certain amount. Default to be below default 15 USD?
+      if (!indexedDB) {
+        console.log('This browser doesn\'t support IndexedDB')
+      } else {
+        let request = indexedDB.open('walletConfig', 2)
+        request.onerror = function (event) {
+          console.log('Database error: ' + event.target.errorCode)
+        }
+        request.onupgradeneeded = function () {
+          let db = request.result
+          console.log('request result', request.result)
+          if (!db.objectStoreNames.contains('whitelist')) {
+            console.log('created?')
+            let store = db.createObjectStore('whitelist', {keyPath: 'id', autoIncrement: true})
+            let index = store.createIndex('domain', 'domain', { unique: true })
+          }
+        }
+        request.onsuccess = function () {
+          let db = request.result
+          console.log('second request', request.result)
+          let transaction = db.transaction(['whitelist'], 'readwrite')
+          let objStore = transaction.objectStore('whitelist')
+          let index = objStore.index('domain')
+          let item = index.get(payment_request_event.paymentRequestOrigin)
+          console.log("item?", item)
+          if (item) {
+            let addRequest = objStore.put({
+              domain: payment_request_event.paymentRequestOrigin,
+              currency: 'USD',
+              capAmount: 15
+            })
+            addRequest.onsuccess = function (event) {
+              console.log('made it!', index.get(payment_request_event.paymentRequestOrigin))
+            }
+          }
+          transaction.oncomplete = function () {
+            db.close()
+          }
+        }
+      }
     }
   })
 }
